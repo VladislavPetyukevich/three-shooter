ce00664b2b64c03ac05fc2258879dca7cc26fe96
VladislavPetyukevich <pet.vladislav@gmail.com>
2018-01-26 21:16:00 +0300

    Add player movement.

--- source/Player.js
+++ source/Player.js
@@ -1,5 +1,7 @@
 var player = {
-	height: 1.8
+	height: 1.8,
+	turnSpeed: Math.PI * 0.015,
+	movementSpeed: 0.25
 };
 
 export default player;
--- source/scenes/scene1.js
+++ source/scenes/scene1.js
@@ -5,15 +5,17 @@ import player from '../Player';
 class Scene1 {
 	constructor(props) {
 		this.scene = new Scene();
-		this.camera = new PerspectiveCamera(75, props.renderWidth / props.renderHeight, 0.1, 1000);
 
+		this.camera = new PerspectiveCamera(75, props.renderWidth / props.renderHeight, 0.1, 1000);
 		this.camera.position.z = 5;
 		this.camera.position.y = player.height;
 		this.camera.lookAt(new Vector3(0, player.height, 0));
+
 		var geometry = new BoxGeometry(1, 1, 1);
 		var material = new MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
 		this.cube = new Mesh(geometry, material);
 		this.scene.add(this.cube);
+
 		this.floor = new Mesh(
 			new PlaneGeometry(50, 50),
 			new MeshBasicMaterial({ color: 0xffffff, wireframe: true })
@@ -26,12 +28,38 @@ class Scene1 {
 		this.cube.rotation.x += 0.01;
 		this.cube.rotation.y += 0.02;
 
+		var camera = this.camera;
+		//left arrow
 		if (controls.keyboard[37]) {
-			this.camera.rotation.y += Math.PI * 0.01;
+			camera.rotation.y += player.turnSpeed;
+		}
+		//right arrow
+		if (controls.keyboard[39]) {
+			camera.rotation.y -= player.turnSpeed;
 		}
 
-		if (controls.keyboard[39]) {
-			this.camera.rotation.y -= Math.PI * 0.01;
+		//W key
+		if (controls.keyboard[87]) {
+			camera.position.x -= Math.sin(camera.rotation.y) * player.movementSpeed;
+			camera.position.z -= Math.cos(camera.rotation.y) * player.movementSpeed;
+		}
+
+		//S key
+		if (controls.keyboard[83]) {
+			camera.position.x += Math.sin(camera.rotation.y) * player.movementSpeed;
+			camera.position.z += Math.cos(camera.rotation.y) * player.movementSpeed;
+		}
+
+		//A key
+		if (controls.keyboard[65]) {
+			camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * player.movementSpeed;
+			camera.position.z += Math.cos(camera.rotation.y - Math.PI / 2) * player.movementSpeed;
+		}
+
+		//D key
+		if (controls.keyboard[68]) {
+			camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.movementSpeed;
+			camera.position.z += Math.cos(camera.rotation.y + Math.PI / 2) * player.movementSpeed;
 		}
 	}
 }
