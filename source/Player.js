44f6503a32c556316dd41c5eb9fa78de8eb59612
VladislavPetyukevich <pet.vladislav@gmail.com>
2018-03-03 17:37:34 +0300

    Refactor Player.

--- source/Player.js
+++ source/Player.js
@@ -1,7 +1,47 @@
-var player = {
-	height: 1.8,
-	turnSpeed: Math.PI * 0.015,
-	movementSpeed: 0.25
-};
+import controls from './Controls';
 
-export default player;
+class Player {
+	constructor(props) {
+		this.height = 1.8;
+		this.turnSpeed = Math.PI * 0.015;
+		this.movementSpeed = 0.25;
+		this.camera = props.camera;
+	}
+
+	update() {
+		var camera = this.camera;
+		if (controls.keyboard[37]) {
+			camera.rotation.y += this.turnSpeed;
+		}
+		//right arrow
+		if (controls.keyboard[39]) {
+			camera.rotation.y -= this.turnSpeed;
+		}
+
+		//W key
+		if (controls.keyboard[87]) {
+			camera.position.x -= Math.sin(camera.rotation.y) * this.movementSpeed;
+			camera.position.z -= Math.cos(camera.rotation.y) * this.movementSpeed;
+		}
+
+		//S key
+		if (controls.keyboard[83]) {
+			camera.position.x += Math.sin(camera.rotation.y) * this.movementSpeed;
+			camera.position.z += Math.cos(camera.rotation.y) * this.movementSpeed;
+		}
+
+		//A key
+		if (controls.keyboard[65]) {
+			camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * this.movementSpeed;
+			camera.position.z += Math.cos(camera.rotation.y - Math.PI / 2) * this.movementSpeed;
+		}
+
+		//D key
+		if (controls.keyboard[68]) {
+			camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * this.movementSpeed;
+			camera.position.z += Math.cos(camera.rotation.y + Math.PI / 2) * this.movementSpeed;
+		}
+	}
+}
+
+export default Player;
--- source/scenes/scene1.js
+++ source/scenes/scene1.js
@@ -1,6 +1,6 @@
 import { Scene, PerspectiveCamera, BoxGeometry, PlaneGeometry, MeshPhongMaterial, PointLight, Mesh, Vector3, AmbientLight } from 'three';
 import controls from '../Controls';
-import player from '../Player';
+import Player from '../Player';
 
 class Scene1 {
 	constructor(props) {
@@ -15,9 +15,10 @@ class Scene1 {
 		this.scene.add(this.pointLight);
 
 		this.camera = new PerspectiveCamera(75, props.renderWidth / props.renderHeight, 0.1, 1000);
+		this.player = new Player({ camera: this.camera });
 		this.camera.position.z = 5;
-		this.camera.position.y = player.height;
-		this.camera.lookAt(new Vector3(0, player.height, 0));
+		this.camera.position.y = this.player.height;
+		this.camera.lookAt(new Vector3(0, this.player.height, 0));
 
 		this.cube = new Mesh(
 			new BoxGeometry(1, 1, 1),
@@ -40,40 +41,7 @@ class Scene1 {
 	update() {
 		this.cube.rotation.x += 0.01;
 		this.cube.rotation.y += 0.02;
-
-		var camera = this.camera;
-		//left arrow
-		if (controls.keyboard[37]) {
-			camera.rotation.y += player.turnSpeed;
-		}
-		//right arrow
-		if (controls.keyboard[39]) {
-			camera.rotation.y -= player.turnSpeed;
-		}
-
-		//W key
-		if (controls.keyboard[87]) {
-			camera.position.x -= Math.sin(camera.rotation.y) * player.movementSpeed;
-			camera.position.z -= Math.cos(camera.rotation.y) * player.movementSpeed;
-		}
-
-		//S key
-		if (controls.keyboard[83]) {
-			camera.position.x += Math.sin(camera.rotation.y) * player.movementSpeed;
-			camera.position.z += Math.cos(camera.rotation.y) * player.movementSpeed;
-		}
-
-		//A key
-		if (controls.keyboard[65]) {
-			camera.position.x += Math.sin(camera.rotation.y - Math.PI / 2) * player.movementSpeed;
-			camera.position.z += Math.cos(camera.rotation.y - Math.PI / 2) * player.movementSpeed;
-		}
-
-		//D key
-		if (controls.keyboard[68]) {
-			camera.position.x += Math.sin(camera.rotation.y + Math.PI / 2) * player.movementSpeed;
-			camera.position.z += Math.cos(camera.rotation.y + Math.PI / 2) * player.movementSpeed;
-		}
+		this.player.update();
 	}
 }
 
