e92fd24a9de64052e6cf5196dd3b8adb44f2f7ca
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-02-17 18:40:27 +0300

    Refactor PhysicsBox

--- source/Physics/PhysicsBox.js
+++ source/Physics/PhysicsBox.js
@@ -11,25 +11,17 @@ export default class PhysicsBox {
   constructor(geometry, material, position = { x: 0, y: 0, z: 0 }, mass = 5) {
     const halfExtents = new Vec3(geometry.parameters.width / 2, geometry.parameters.height / 2, geometry.parameters.depth / 2);
     const boxShape = new Box(halfExtents);
-    this.boxBody = new Body({ mass });
-    this.boxBody.addShape(boxShape);
-    this.boxMesh = new Mesh(geometry, material);
-    this.boxBody.position.set(position.x, position.y, position.z);
-    this.boxMesh.position.set(position.x, position.y, position.z);
-    this.boxMesh.castShadow = true;
-    this.boxMesh.receiveShadow = true;
-  }
-
-  get body() {
-    return this.boxBody;
-  }
-
-  get mesh() {
-    return this.boxMesh;
+    this.body = new Body({ mass });
+    this.body.addShape(boxShape);
+    this.mesh = new Mesh(geometry, material);
+    this.body.position.set(position.x, position.y, position.z);
+    this.mesh.position.set(position.x, position.y, position.z);
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
   }
 
   update() {
-    this.boxMesh.position.copy(this.boxBody.position);
-    this.boxMesh.quaternion.copy(this.boxBody.quaternion);
+    this.mesh.position.copy(this.body.position);
+    this.mesh.quaternion.copy(this.body.quaternion);
   }
 }
