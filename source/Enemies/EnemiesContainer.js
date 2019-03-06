fdf3f04deef4a777c78e5a6451e828ed20e44678
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-03-06 23:31:17 +0300

    Refactor Enemy class

--- source/Enemies/EnemiesContainer.js
+++ source/Enemies/EnemiesContainer.js
@@ -6,21 +6,21 @@ export default class EnemyContainer {
   }
 
   add(enemy) {
-    enemy.enemy.body.addEventListener("collide", event => {
+    enemy.body.addEventListener("collide", event => {
       if (event.body.isBullet) {
         event.target._hp--;
       }
     });
     this.enemies.push(enemy);
-    this.world.addBody(enemy.enemy.body);
-    this.scene.add(enemy.enemy.mesh);
+    this.world.addBody(enemy.body);
+    this.scene.add(enemy.mesh);
   }
 
   deleteEnemyByUuid(uuid) {
     const enemies = this.enemies;
     for (var i = enemies.length; i--;) {
       const enemy = enemies[i];
-      if (enemy.enemy.mesh.uuid === uuid) {
+      if (enemy.mesh.uuid === uuid) {
         this.enemies.splice(i, 1);
         return;
       }
@@ -29,11 +29,11 @@ export default class EnemyContainer {
 
   update() {
     this.enemies.forEach(enemy => enemy.update());
-    const enemiesToDelete = this.enemies.filter(enemy => enemy.enemy.body._hp <= 0);
+    const enemiesToDelete = this.enemies.filter(enemy => enemy.body._hp <= 0);
     enemiesToDelete.forEach(enemy => {
-      this.scene.remove(enemy.enemy.mesh);
-      this.world.remove(enemy.enemy.body);
-      this.deleteEnemyByUuid(enemy.enemy.mesh.uuid);
+      this.scene.remove(enemy.mesh);
+      this.world.remove(enemy.body);
+      this.deleteEnemyByUuid(enemy.mesh.uuid);
     });
   }
 }
--- source/Enemies/Enemy.js
+++ source/Enemies/Enemy.js
@@ -5,42 +5,39 @@ import enemyTexture from '../assets/golem.png';
 
 const textureLoader = new TextureLoader();
 
-export default class Enemy {
+export default class Enemy extends PhysicsBox {
   constructor(props) {
-    this.playerBody = props.playerBody;
-    this.walkSpeed = 10;
-
     const spriteMap = textureLoader.load(enemyTexture);
     const geometry = new BoxGeometry(3, 3, 1);
-
     const material = new MeshLambertMaterial({
       map: spriteMap
     });
     material.transparent = true;
-    this.enemy = new PhysicsBox(
+    super(
       geometry,
       [null, null, null, null, material],
       props.position
     );
-    this.enemy.body.collisionResponse = true;
-    this.enemy.body._hp = 1;
-    this.enemy.body.isEnemy = true;
-    this.enemy.mesh.receiveShadow = true;
+  
+    this.playerBody = props.playerBody;
+    this.walkSpeed = 10;
+    this.body.collisionResponse = true;
+    this.body._hp = 1;
+    this.body.isEnemy = true;
+    this.mesh.receiveShadow = true;
     this.inputVelocity = new Vector3();
     this.euler = new Euler();
     this.quat = new Quaternion();
   }
 
-  getObject = () => this.enemy;
-
   update() {
+    super.update();
     const direction = new Vec3();
-    this.playerBody.position.vsub(this.enemy.body.position, direction);
+    this.playerBody.position.vsub(this.body.position, direction);
     direction.y = 0;
     direction.normalize();
     const forward = new Vec3(0, 0, 1);
-    this.enemy.body.quaternion.setFromVectors(forward, direction);
-    direction.mult(this.walkSpeed, this.enemy.body.velocity);
-    this.enemy.update();
+    this.body.quaternion.setFromVectors(forward, direction);
+    direction.mult(this.walkSpeed, this.body.velocity);
   }
 }
