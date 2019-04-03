076af69090ad55b7248983b6f1ac8a35e48f8eff
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-04-04 00:17:58 +0300

    Create Player actor

---
+++ source/Enemies/Player.js
@@ -0,0 +1,25 @@
+import { Vector3 } from 'three';
+import InvisPhysicsSphere from '../Physics/InvisPhysicsSphere';
+import Actor from '../Actor';
+
+const WALK_SPEED = 25;
+
+export default class Player extends Actor {
+  constructor(props) {
+    const sphereRadius = 1.3;
+    const sphereMass = 10;
+    super({
+      hp: 1,
+      solidBody: new InvisPhysicsSphere(
+        sphereRadius, props.position, sphereMass, new Vector3(0, 10, 0)
+      ),
+      walkSpeed: WALK_SPEED
+    });
+
+    this.solidBody.body.linearDamping = 0.9;
+  }
+
+  update(delta) {
+    super.update(delta);
+  }
+}
