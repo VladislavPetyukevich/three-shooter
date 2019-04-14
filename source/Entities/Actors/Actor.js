232a54e9ac7601ed3a52ec73517b0665bfad4549
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-04-14 20:09:25 +0300

    Refactor hp field in Actor

--- source/Entities/Actors/Actor.js
+++ source/Entities/Actors/Actor.js
@@ -1,11 +1,10 @@
 export default class Actor {
   constructor(props) {
-    this.hp = props.hp;
     this.solidBody = props.solidBody;
     this.walkSpeed = props.walkSpeed;
     this.gun = props.gun;
 
-    this.solidBody.body._hp = 1;
+    this.solidBody.body._hp = props.hp;
   }
 
   update(delta) {
