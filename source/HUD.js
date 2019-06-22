e1fd6526314755b45d666679de3e511f30917586
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-06-22 20:50:55 +0300

    Remove crosshair from HUD

--- source/HUD.js
+++ source/HUD.js
@@ -1,5 +1,4 @@
 import { TextureLoader, Scene, OrthographicCamera, SpriteMaterial, Sprite } from 'three';
-import crosshairFile from './assets/crosshair.png';
 
 const textureLoader = new TextureLoader();
 
@@ -9,12 +8,6 @@ class HUD {
     const width = window.innerWidth;
     const height = window.innerHeight;
     this.camera = new OrthographicCamera(-width, width, height, -height, - 500, 1000);
-
-    const crosshairTexture = textureLoader.load(crosshairFile);
-    const crosshairMaterial = new SpriteMaterial({ map: crosshairTexture });
-    const crosshair = new Sprite(crosshairMaterial);
-    crosshair.scale.set(39, 39);
-    this.scene.add(crosshair);
   }
 }
 
