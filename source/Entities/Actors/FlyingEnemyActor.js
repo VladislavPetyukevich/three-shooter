9a9118046eea1bc5a5621f54fcef834f65ef87c4
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-08-10 22:21:02 +0300

    Enemies MeshPhongMaterial

--- source/Entities/Actors/FlyingEnemyActor.js
+++ source/Entities/Actors/FlyingEnemyActor.js
@@ -1,4 +1,4 @@
-import { TextureLoader, MeshLambertMaterial, BoxGeometry } from 'three';
+import { TextureLoader, MeshPhongMaterial, BoxGeometry } from 'three';
 import Actor from './Actor';
 import PhysicsBox from '../../Physics/PhysicsBox';
 import TextureAnimator from '../../TextureAnimator';
@@ -12,10 +12,9 @@ const WALK_TEXTURE_TILES_VERTICAL = 1;
 export default class EnemyActor extends Actor {
   constructor(playerBody, position = { x: 0, y: 0, z: 0 }) {
     const spriteMap = textureLoader.load(enemyTexture);
-    spriteMap.anisotropy = 16;
     const geometry = new BoxGeometry(3, 3, 1);
     const mass = 0.1;
-    const material = new MeshLambertMaterial({
+    const material = new MeshPhongMaterial({
       map: spriteMap
     });
     material.transparent = true;
