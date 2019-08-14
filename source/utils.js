597a96f99e164085902a23d189babfd5eb2bb4c9
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-08-14 23:47:07 +0300

    Migrate utils to typescript

--- source/utils.js
+++
@@ -1,3 +0,0 @@
-export const PI_180 = Math.PI / 180;
-
-export const toRadians = degrees => degrees * PI_180;
---
+++ source/utils.ts
@@ -0,0 +1,3 @@
+export const PI_180 = Math.PI / 180;
+
+export const toRadians = (degrees: number) => degrees * PI_180;
