d8e389eb65767859eaf9313027018149bf33bcd5
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-08-20 22:21:22 +0300

    Convert Sounds to TypeScript

--- source/Entities/Sounds/Sounds.js
+++
@@ -1,7 +0,0 @@
-export default class Sounds {
-  constructor(audioListener, actor, soundsBuffer) {
-    this.audioListener = audioListener;
-    this.actor = actor;
-    this.soundsBuffer = soundsBuffer;
-  }
-}
---
+++ source/Entities/Sounds/Sounds.ts
@@ -0,0 +1,15 @@
+import { AudioListener } from 'three';
+import Actor from '../Actors/Actor';
+import SoundsBuffer from './SoundsBuffer';
+
+export default class Sounds {
+  audioListener: AudioListener;
+  actor: Actor;
+  soundsBuffer: SoundsBuffer;
+
+  constructor(audioListener: AudioListener, actor: Actor, soundsBuffer: SoundsBuffer) {
+    this.audioListener = audioListener;
+    this.actor = actor;
+    this.soundsBuffer = soundsBuffer;
+  }
+}
