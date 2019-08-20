5b16672c5708a2058dca3e01e24ba5c027836c77
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-08-20 22:20:37 +0300

    Convert EnemySounds to TypeScript

--- source/Entities/Sounds/EnemySounds.ts
+++ source/Entities/Sounds/EnemySounds.ts
@@ -4,6 +4,10 @@ import { ENEMY } from '../../constants';
 
 export default class EnemySounds extends Sounds {
   shoot() {
+    if (!this.actor.solidBody.mesh) {
+      throw new Error('EnemySounds: Actor mesh is not specified');
+    }
+
     const audio = new PositionalAudio(this.audioListener);
     audio.setBuffer(this.soundsBuffer.buffers[ENEMY.SHOOT_SOUND_INDEX]);
     this.actor.solidBody.mesh.add(audio);
