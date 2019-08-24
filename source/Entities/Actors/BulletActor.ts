86c2914903c96e67a6bd64f089c7ba49c6d43908
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-08-24 12:47:29 +0300

    Create PhysicsBullet

--- source/Entities/Actors/BulletActor.ts
+++ source/Entities/Actors/BulletActor.ts
@@ -1,21 +1,8 @@
-import { SphereGeometry, MeshBasicMaterial } from 'three';
 import Actor from './Actor';
-import PhysicsBall from '../../SolidBody/PhysicsBall';
-import { BULLET } from '../../constants';
+import PhysicsBullet from '../../SolidBody/PhysicsBullet';
 
 export default class BulletActor extends Actor {
   constructor(position = { x: 0, y: 0, z: 0 }) {
-    const solidBody = new PhysicsBall(
-      new SphereGeometry(BULLET.SHAPE_RADIUS, 8, 8),
-      new MeshBasicMaterial({ color: BULLET.COLOR }),
-      position,
-      BULLET.MASS
-    );
-
-    super({
-      solidBody
-    });
-
-    // this.solidBody.body!.isBullet = true;
+    super({ solidBody: new PhysicsBullet(position) });
   }
 }
---
+++ source/SolidBody/PhysicsBullet.ts
@@ -0,0 +1,20 @@
+import { SphereGeometry, MeshBasicMaterial } from 'three';
+import { Body } from 'cannon';
+import PhysicsBall from './PhysicsBall';
+import { BULLET } from '../constants';
+
+export interface BulletBody extends Body {
+  isBullet?: true;
+}
+
+export default class PhysicsBullet extends PhysicsBall {
+  constructor(position = { x: 0, y: 0, z: 0 }) {
+    super(
+      new SphereGeometry(BULLET.SHAPE_RADIUS, 8, 8),
+      new MeshBasicMaterial({ color: BULLET.COLOR }),
+      position,
+      BULLET.MASS
+    );
+    (<BulletBody>this.body!).isBullet = true;
+  }
+}
