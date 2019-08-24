99da3cf2f5e5d96a9c706ba2aa6da2fe7c2c85bb
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-08-24 12:44:09 +0300

    Add hp field in Entity

--- source/Entities/Entity.ts
+++ source/Entities/Entity.ts
@@ -6,11 +6,13 @@ export default class Entity {
   type: ENTITY_TYPE;
   actor: Actor;
   behavior: Behavior;
+  hp?: number;
 
-  constructor(type: ENTITY_TYPE, actor: Actor, behavior: Behavior) {
+  constructor(type: ENTITY_TYPE, actor: Actor, behavior: Behavior, hp?: number) {
     this.type = type;
     this.actor = actor;
     this.behavior = behavior;
+    this.hp = hp;
   }
 
   update(delta: number) {
