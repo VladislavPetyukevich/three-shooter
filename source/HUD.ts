08ad310aad257813fc791ac21dec0e4fb6f00efd
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-11-08 17:39:02 +0300

    Add HUD gun

--- source/HUD.ts
+++ source/HUD.ts
@@ -1,4 +1,5 @@
 import { TextureLoader, Scene, OrthographicCamera, SpriteMaterial, Sprite } from 'three';
+import gunTextureFile from './assets/gun.png';
 
 const textureLoader = new TextureLoader();
 
@@ -11,5 +12,14 @@ export class HUD {
     const width = window.innerWidth;
     const height = window.innerHeight;
     this.camera = new OrthographicCamera(-width, width, height, -height, - 500, 1000);
+
+    const gunTexture = textureLoader.load(gunTextureFile);
+    const gunMaterial = new SpriteMaterial({ map: gunTexture });
+    const gun = new Sprite(gunMaterial);
+    const gunWidth = width * 0.5;
+    const gunHeight = width * 0.5;
+    gun.scale.set(gunWidth, gunHeight, 1);
+    gun.position.set(0.5, -height + gunHeight / 2, 1);
+    this.scene.add(gun);
   }
 }
