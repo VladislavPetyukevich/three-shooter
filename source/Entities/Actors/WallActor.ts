8bb83dab8a7b2346232322dedc1ae9b77f485e08
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-12-21 12:31:43 +0300

    Add wall texture

--- source/Entities/Actors/WallActor.ts
+++ source/Entities/Actors/WallActor.ts
@@ -1,5 +1,7 @@
 import { Actor } from '@/core/Entities/Actor';
 import { Mesh, BoxGeometry, MeshPhongMaterial, Vector3 } from 'three';
+import { textureLoader } from '@/TextureLoader';
+import wallTextureFile from '@/assets/wall.png';
 
 interface WallActorProps {
   size: { width: number; height: number, depth: number };
@@ -10,8 +12,9 @@ export class WallActor implements Actor {
   mesh: Mesh;
 
   constructor(props: WallActorProps) {
+    const texture = textureLoader.load(wallTextureFile);
     const geometry = new BoxGeometry(props.size.width, props.size.height, props.size.depth);
-    const material = new MeshPhongMaterial({ color: 'blue' });
+    const material = new MeshPhongMaterial({ map: texture });
     this.mesh = new Mesh(geometry, material);
     this.mesh.position.set(
       props.position.x,
