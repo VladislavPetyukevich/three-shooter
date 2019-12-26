72af705a2c31589c9977d6bb7c127cb93d1ea3d3
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-12-26 21:57:16 +0300

    New wall texture and normal map

--- source/Entities/Wall/WallActor.ts
+++ source/Entities/Wall/WallActor.ts
@@ -2,6 +2,7 @@ import { Actor } from '@/core/Entities/Actor';
 import { Mesh, BoxGeometry, MeshPhongMaterial, Vector3 } from 'three';
 import { textureLoader } from '@/TextureLoader';
 import wallTextureFile from '@/assets/wall.png';
+import wallNormalFile from '@/assets/wall-normal.png';
 
 interface WallActorProps {
   size: { width: number; height: number, depth: number };
@@ -13,8 +14,12 @@ export class WallActor implements Actor {
 
   constructor(props: WallActorProps) {
     const texture = textureLoader.load(wallTextureFile);
+    const normal = textureLoader.load(wallNormalFile);
     const geometry = new BoxGeometry(props.size.width, props.size.height, props.size.depth);
-    const material = new MeshPhongMaterial({ map: texture });
+    const material = new MeshPhongMaterial({
+      map: texture,
+      normalMap: normal
+    });
     this.mesh = new Mesh(geometry, material);
     this.mesh.position.set(
       props.position.x,
