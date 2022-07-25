3e1b0a71a313f312c42ecbf706c84dd6c17b345b
VladislavPetyukevich <pet.vladislav@gmail.com>
2022-07-25 22:49:10 +0300

    Check raycast collisions in all cases

--- source/core/Entities/EntitiesContainer.ts
+++ source/core/Entities/EntitiesContainer.ts
@@ -69,61 +69,55 @@ export class EntitiesContainer {
       );
       const newPositionX = entity.actor.mesh.position.x + entity.velocity.x * delta;
       const newPositionZ = entity.actor.mesh.position.z + entity.velocity.z * delta;
-      if (delta < 0.066) { // > 15 fps
-        this.updateEntityPosition(
-          entity,
-          new Vector3(
-            newPositionX,
-            entity.actor.mesh.position.y,
-            entity.actor.mesh.position.z
-          )
-        );
-        this.updateEntityPosition(
-          entity,
-          new Vector3(
-            entity.actor.mesh.position.x,
-            entity.actor.mesh.position.y,
-            newPositionZ
-          )
-        );
-      } else {
-        this.updateEntityPositionRaycast(
-          entity,
-          new Vector3(
-            newPositionX,
-            entity.actor.mesh.position.y,
-            entity.actor.mesh.position.z
-          )
-        );
-        this.updateEntityPositionRaycast(
-          entity,
-          new Vector3(
-            entity.actor.mesh.position.x,
-            entity.actor.mesh.position.y,
-            newPositionZ
-          )
-        );
-      }
+      this.updateEntityPosition(
+        entity,
+        new Vector3(
+          newPositionX,
+          entity.actor.mesh.position.y,
+          entity.actor.mesh.position.z
+        )
+      );
+      this.updateEntityPosition(
+        entity,
+        new Vector3(
+          entity.actor.mesh.position.x,
+          entity.actor.mesh.position.y,
+          newPositionZ
+        )
+      );
       entity.update(delta);
     });
   }
 
-  updateEntityPositionRaycast(entity: Entity, newPosition: Vector3) {
-    const collisionsResult = this.collideCheckerRaycast.detectCollisions(entity, newPosition);
-    if (collisionsResult.length === 0) {
-      this.collideChecker.updateEntityPosition(entity, newPosition);
-    }
-  }
-
   updateEntityPosition(entity: Entity, newPosition: Vector3) {
-    const collisionsResult = this.collideChecker.detectCollisions(entity, newPosition);
-    if (collisionsResult.entities.length === 0) {
+    const collisionsResult: Entity[] = [];
+    collisionsResult.push(
+      ...this.collideChecker.detectCollisions(entity, newPosition)
+    );
+    const collisionsResultRaycast = this.collideCheckerRaycast.detectCollisions(entity, newPosition);
+    collisionsResultRaycast.forEach(collisionRaycast => {
+      const meshId = collisionRaycast.object.id;
+      const isDetectedPreviously = collisionsResult.some(
+        collision => collision.actor.mesh.id === meshId
+      );
+      if (isDetectedPreviously) {
+        return;
+      }
+      const entity = this.meshIdToEntity.get(meshId);
+      if (!entity) {
+        console.warn('Collided entity not found by mesh id');
+        return;
+      }
+      collisionsResult.push(entity);
+    });
+    if (collisionsResult.length === 0) {
       this.collideChecker.updateEntityPosition(entity, newPosition);
       return;
     }
 
+
     let isEntityCanMove = true;
-    collisionsResult.entities.forEach(collideEntity => {
+    collisionsResult.forEach(collideEntity => {
       collideEntity.onCollide(entity);
       const collideResult = entity.onCollide(collideEntity);
       if (!collideResult) {
