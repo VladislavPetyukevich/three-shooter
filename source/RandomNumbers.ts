95604abb6169793ef7ec6fae9854cb250805fccf
VladislavPetyukevich <pet.vladislav@gmail.com>
2022-12-01 00:02:14 +0300

    Seedable random

--- source/RandomNumbers.ts
+++ source/RandomNumbers.ts
@@ -1,13 +1,23 @@
 import { RANDOM_NUMBERS_COUNT } from '@/constants';
 
 class RandomNumbers {
+  m: number;
+  a: number;
+  c: number;
+  seed: number;
+  state: number;
   values: number[];
   i: number;
 
   constructor(size: number) {
+    this.m = 0x80000000;
+    this.a = 1103515245;
+    this.c = 12345;
+    this.seed = Math.floor(Math.random() * (this.m - 1));
+    this.state = this.seed;
     this.values = Array.from(
       { length: size },
-      Math.random
+      () => this.RNGNextFloat()
     );
     this.i = 0;
   }
@@ -22,6 +32,11 @@ class RandomNumbers {
 
   getRandomInRange(min: number, max: number) {
     return Math.floor(this.getRandom() * (max - min + 1)) + min;
+  }
+
+  private RNGNextFloat() {
+    this.state = (this.a * this.state + this.c) % this.m;
+    return this.state / (this.m - 1);
   }
 }
 
